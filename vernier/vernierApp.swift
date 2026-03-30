a367969b5d8848bf0c81c9ae3edca24d3890c3a8
rishi v <74287374+rishi4x@users.noreply.github.com>
2026-03-30 18:23:19 +0530

    Initial Commit

---
+++ vernier/vernierApp.swift
@@ -0,0 +1,17 @@
+//
+//  vernierApp.swift
+//  vernier
+//
+//  Created by Rishi V on 30/03/26.
+//
+
+import SwiftUI
+
+@main
+struct vernierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
